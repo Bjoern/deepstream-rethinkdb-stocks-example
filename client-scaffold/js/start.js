2446791e4234a25e3d517dfa77025378a5a2b7e2
Björn Günzel <guenzel.bjoern@gmail.com>
2015-11-25 20:43:18 +0100

    creating agents for all permutations of rules

--- client-scaffold/js/start.js
+++ client-scaffold/js/start.js
@@ -1,13 +1,72 @@
 ds = deepstream('52.29.184.11:6020').login({}, onConnected );
+var symbols = [
+	"MMM",
+	"ABT",
+	"ABBV",
+	"ACN",
+	"ACE",
+	"ATVI",
+	"ADBE",
+	"ADT",
+	"AAP",
+	"AES",
+	"AET",
+	"AFL",
+	"AMG",
+	"A",
+	"GAS",
+	"APD",
+	"ARG",
+	"AKAM",
+	"AA",
+	"AGN",
+	"ALXN",
+	"ALLE",
+	"ADS",
+	"ALL",
+	"GOOGL",
+	"GOOG",
+	"ALTR",
+	"MO",
+	"AMZN",
+	"AEE",
+	"AAL",
+	"AEP",
+	"AXP",
+	"AIG",
+	"AMT",
+	"AMP",
+	"ABC",
+	"AME",
+	"AMGN",
+	"APH",
+	"APC",
+	"ADI",
+	"AON",
+	"APA",
+	"AIV",
+	"AAPL",
+	"AMAT",
+	"ADM",
+  "AIZ"];
 
 function onConnected() {
-	var amzn = ds.record.getRecord( 'sp500/AMZN' );
-	
-	amzn.subscribe( 'company_name', function( companyName ){
-		$( '.company-name' ).text( companyName );
-	});
+  var count = 20;
 
-	amzn.subscribe( 'last_price', function( lastPrice ){
-		$( '.price' ).text( Number( lastPrice ).toFixed( 2 ) );
-	});
-}
+  for(var i = 0; i < symbols.length; i++){
+    var symbol = symbols[i];
+    var record = ds.record.getRecord( 'sp500/'+symbol);
+    record.subscribe( 'company_name', function( companyName ){
+        console.log("company-name: "+companyName);
+        //$( '.company-name' ).text( companyName );
+           
+        record.subscribe( 'last_price', function( lastPrice ){
+            console.log("lastPrice: "+lastPrice);
+            //$( '.price' ).text( Number( lastPrice ).toFixed( 2 ) );
+          });
+
+
+      });
+
+     }
+  }
